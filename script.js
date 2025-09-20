41b4b92a3f9aa80d2ae7ab31fe082cbe1c6fa115
Lipatech <yuliia.vovk.pp.2024@lpnu.ua>
2025-09-20 13:09:27 +0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// document.querySelector('.emojies').addEventListener('click', function(e) {
-//     e.preventDefault();
-//     document.querySelector('.sub-dropdown-content').classList.toggle('show');
-//   });
-  
 const dropdownBtn = document.querySelector(".dropdown");
 const dropdownContent = document.querySelector(".dropdown-content");
 
 dropdownBtn.addEventListener("click", () => {
   dropdownContent.classList.toggle("show");
 });
+
