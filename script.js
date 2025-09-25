c67ab23a837a65d2c81ed9a3faaf293f7f9648d6
Yu <y42668345@gmail.com>
2025-09-25 19:42:08 +0300

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ dropdownBtn.addEventListener("click", () => {
   dropdownContent.classList.toggle("show");
 });
 
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".categories button");
+    const images = document.querySelectorAll("main img");
+  
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        const category = button.getAttribute("data-category");
+  
+        // спершу прибираємо highlight з усіх
+        images.forEach(img => img.classList.remove("highlight"));
+  
+        // додаємо highlight лише потрібним
+        images.forEach(img => {
+          if (img.id === category) {
+            img.classList.add("highlight");
+          }
+        });
+      });
+    });
+  });
+  
